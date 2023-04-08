6c7a62f2a9abf8cdd7f29ea57ae79a42adb9fc65
Elliott <tung.elliott@gmail.com>
2023-04-08 15:09:22 -0700

    complete /reviews route

--- routes.js
+++ routes.js
@@ -11,10 +11,63 @@ const pool = new Pool({
 
 router.get('/reviews', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM reviews WHERE id < 10');
-    res.status(200).json(result.rows);
+    const { page = 1, count = 5, sort, product_id } = req.query;
+
+    if (!product_id) {
+      return res.status(400).send('Missing required query parameter: product_id');
+    }
+
+    const offset = (page - 1) * count;
+
+    let sortQuery = 'ORDER BY date DESC'; // Default sort by newest
+
+    if (sort === 'helpful') {
+      sortQuery = 'ORDER BY helpfulness DESC';
+    } else if (sort === 'relevant') {
+      sortQuery = `
+        ORDER BY (
+          0.3 * (EXTRACT(EPOCH FROM (NOW() - date)) / (60 * 60 * 24 * 365)) +
+          0.5 * (helpfulness / GREATEST(helpfulness + 1, 1)) +
+          0.2 * (rating / 5)
+        ) DESC
+      `;
+    }
+
+    const reviewQuery = `
+      SELECT r.id as review_id, r.rating, r.summary, r.recommend, r.response, r.body, r.date, r.reviewer_name, r.helpfulness
+      FROM reviews r
+      WHERE r.product_id = $1 AND r.reported = false
+      ${sortQuery}
+      LIMIT $2 OFFSET $3
+    `;
+    const result = await pool.query(reviewQuery, [product_id, count, offset]);
+
+    const reviewIds = result.rows.map(row => row.review_id);
+    const photoQuery = `
+      SELECT rp.id, rp.review_id, rp.url
+      FROM review_photos rp
+      WHERE rp.review_id = ANY ($1)
+    `;
+    const photoResult = await pool.query(photoQuery, [reviewIds]);
+
+    const reviewsWithPhotos = result.rows.map(row => {
+      const photos = photoResult.rows.filter(photo => photo.review_id === row.review_id);
+      return {
+        ...row,
+        photos,
+      };
+    });
+
+    const response = {
+      product: product_id,
+      page: parseInt(page),
+      count: parseInt(count),
+      results: reviewsWithPhotos,
+    };
+
+    res.status(200).json(response);
   } catch (error) {
-    console.error(error, 'help');
+    console.error(error);
     res.status(500).send('An error occurred while fetching reviews');
   }
 });
